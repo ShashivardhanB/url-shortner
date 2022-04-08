198008de2c5193b47b538c4b97b6c4c7ab0b7337
ShashivardhanB <itsmeshashivardhanreddy8340@gmail.com>
2022-04-08 10:03:28 +0530

    project url-shortner

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -4,8 +4,7 @@ const shortid = require("shortid");
 const redis = require("redis");
 const { promisify } = require("util")
 
-
-const urlReg = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/
+const urlReg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 const isValid = function (value) {
     if (typeof value === 'undefined' || value === null) return false
     if (typeof value === 'string' && value.trim().length === 0) return false
@@ -18,25 +17,25 @@ const isValidRequestBody = function (requestBody) {
 }
 // -----------------------------------------------------------------------------------------------------
 
-const redisClient =  redis.createClient(
+const redisClient = redis.createClient(
     11826,
     "redis-11826.c212.ap-south-1-1.ec2.cloud.redislabs.com",
     { no_ready_check: true }
-  );
-  redisClient.auth("eZihcVKVefVZj7J7gYZADiJYJE8Kubal", function (err) {
+);
+redisClient.auth("eZihcVKVefVZj7J7gYZADiJYJE8Kubal", function (err) {
     if (err) throw err;
-  });
-     redisClient.on("connect", async function () {
+});
+redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
-  });
-  
+});
+
+
 //1. connect to the server
 //2. use the commands :
 //Connection setup for redis
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-
 
 // ---------------------------------------------------------------------------------------------------------------------
 const createShortUrl = async function (req, res) {
@@ -64,10 +63,13 @@ const createShortUrl = async function (req, res) {
         const isUrlAlreadyShorten = await urlModel.findOne({ longUrl: longUrl }).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
 
         if (isUrlAlreadyShorten) {
+            await SET_ASYNC(`${isUrlAlreadyShorten.urlCode}`, JSON.stringify(isUrlAlreadyShorten.longUrl))
             return res.status(200).send({ status: true, data: isUrlAlreadyShorten })
         }
 
         const SavedUrl = await urlModel.create(requestBody)
+
+        await SET_ASYNC(`${requestBody.urlCode}`, JSON.stringify(requestBody.longUrl))
 
         return res.status(201).send({
             status: true, data: {
@@ -90,23 +92,24 @@ const originalUrl = async function (req, res) {
             return res.status(400).send({ status: false, message: "please provide urlcode" })
         }
 
-       const  cahcedOrginalUrl = await GET_ASYNC(`${urlCode}`)
+        const cahcedOrginalUrl = await GET_ASYNC(`${urlCode}`)
+        if (cahcedOrginalUrl) {
 
-        if(cahcedOrginalUrl){
-          let  cahcedOrginalUrltoObject = JSON.parse(cahcedOrginalUrl)
-            return res.status(302).redirect(cahcedOrginalUrltoObject.longUrl)
-        }
-        else
-        {
-        const urlData = await urlModel.findOne({ urlCode: urlCode })
-        if (!urlData) {
-            return res.status(404).send({ status: false, message: "url not found" })
+            let redirectingData = JSON.parse(cahcedOrginalUrl)
+
+            return res.status(302).redirect(redirectingData)
+        } else { 
+
+            const urlData = await urlModel.findOne({ urlCode: urlCode })
+            if (!urlData) {
+                return res.status(404).send({ status: false, message: "url not found" })
         }
 
-        await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData))
-        return res.status(302).redirect(urlData.longUrl)
-        
-    }
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData.longUrl))
+
+            return res.status(302).redirect(urlData.longUrl)
+
+        }
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }
