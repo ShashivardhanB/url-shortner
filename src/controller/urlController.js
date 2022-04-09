cd822acd0ffca0dc9512c78e30778922e009b279
ShashivardhanB <itsmeshashivardhanreddy8340@gmail.com>
2022-04-09 17:03:47 +0530

    project urlShortner

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -4,18 +4,18 @@ const shortid = require("shortid");
 const redis = require("redis");
 const { promisify } = require("util")
 
-const urlReg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+
+const urlReg = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/
+
 const isValid = function (value) {
     if (typeof value === 'undefined' || value === null) return false
     if (typeof value === 'string' && value.trim().length === 0) return false
-    if (typeof value === 'number' && value.toString().trim().length === 0) return false
     return true;
 }
-
 const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
 }
-// -----------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------
 
 const redisClient = redis.createClient(
     11826,
@@ -29,10 +29,10 @@ redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
 });
 
-
 //1. connect to the server
 //2. use the commands :
 //Connection setup for redis
+// Promisify is used when you want to convert a callback function into a promise based function
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
@@ -56,16 +56,18 @@ const createShortUrl = async function (req, res) {
             return res.status(400).send({ status: false, message: "please enter valid url " })
         }
 
-        requestBody["urlCode"] = shortid.generate().toLowerCase()
-
-        requestBody["shortUrl"] = `http://localhost:3000/${requestBody.urlCode}`
-
+        // checking that longUrl is already shortner
         const isUrlAlreadyShorten = await urlModel.findOne({ longUrl: longUrl }).select({ __v: 0, createdAt: 0, updatedAt: 0, _id: 0 })
 
         if (isUrlAlreadyShorten) {
-            await SET_ASYNC(`${isUrlAlreadyShorten.urlCode}`, JSON.stringify(isUrlAlreadyShorten.longUrl))
+            await SET_ASYNC(`${isUrlAlreadyShorten.urlCode}`, JSON.stringify(isUrlAlreadyShorten.longUrl))      
+
             return res.status(200).send({ status: true, data: isUrlAlreadyShorten })
         }
+
+        requestBody["urlCode"] = shortid.generate().toLowerCase()
+
+        requestBody["shortUrl"] = `http://localhost:3000/${requestBody.urlCode}`            
 
         const SavedUrl = await urlModel.create(requestBody)
 
@@ -86,29 +88,27 @@ const createShortUrl = async function (req, res) {
 
 const originalUrl = async function (req, res) {
     try {
+
         const urlCode = req.params.urlCode
 
         if (!isValid(urlCode)) {
             return res.status(400).send({ status: false, message: "please provide urlcode" })
         }
-
         const cahcedOrginalUrl = await GET_ASYNC(`${urlCode}`)
         if (cahcedOrginalUrl) {
-
             let redirectingData = JSON.parse(cahcedOrginalUrl)
 
-            return res.status(302).redirect(redirectingData)
-        } else { 
+            console.log("data from cachee")
 
+            return res.status(302).redirect(redirectingData)
+        } else {
             const urlData = await urlModel.findOne({ urlCode: urlCode })
             if (!urlData) {
                 return res.status(404).send({ status: false, message: "url not found" })
-        }
-
+            }
             await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData.longUrl))
-
+            console.log("data from db")
             return res.status(302).redirect(urlData.longUrl)
-
         }
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
@@ -118,3 +118,6 @@ const originalUrl = async function (req, res) {
 module.exports = { createShortUrl, originalUrl }
 
 
+
+// Least Frequently Used
+// ,"EX",30s
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
 const route = require('./routes/route');
 const  mongoose  = require('mongoose');
 const app = express();
@@ -9,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 // basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+
 
 mongoose.connect("mongodb+srv://sumandev:aBosU15RXTGZYkKq@cluster0.4du2i.mongodb.net/group76database?retryWrites=true&w=majority", {
     useNewUrlParser: true,useUnifiedTopology: true
@@ -17,10 +16,7 @@ mongoose.connect("mongodb+srv://sumandev:aBosU15RXTGZYkKq@cluster0.4du2i.mongodb
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-
-
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
